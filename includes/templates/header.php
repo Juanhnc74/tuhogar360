c6368d908f311d68c0d5784066ed4a58edbc4ec2
Arthurssj18 <147117793+Arthurssj18@users.noreply.github.com>
2023-12-03 13:24:40 -0600

    Add files via upload

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -9,7 +9,7 @@
 </head>
 <body>
     
-    <header class= "header <?php echo $inicio ? 'inicio' : '';  ?>">
+    <header class= "header <?php echo isset ($inicio) ? 'inicio' : '';  ?>">
         <div class="contenedor contenido-header">
             <div class="barra">
                 <a href="/tuhogar360-main/index.php">
