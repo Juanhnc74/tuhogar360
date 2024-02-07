7e3de26acac8919e2406dce7f91be8828cf2f977
juanhnc74 <juanhnc74@gmail.com>
2024-02-07 14:04:00 -0600

    Proyecto inicial

--- includes/templates/config/database.php
+++ includes/templates/config/database.php
@@ -1,7 +1,7 @@
 <?php
 
 function conectarDB(): mysqli {
-    $db = mysqli_connect('localhost','root','','bienesraices_crud');
+    $db = mysqli_connect('localhost','root','','crud_bienesraices');
 
     if(!$db){
         echo "Error no se pudo conectar";
