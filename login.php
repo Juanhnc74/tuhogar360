b7f7e9851fedd0c726ec6e581c53a0242302cf34
Arthurssj18 <147117793+Arthurssj18@users.noreply.github.com>
2024-03-04 15:19:28 -0600

    Add files via upload

--- login.php
+++ login.php
@@ -1,170 +1,68 @@
-<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title> Responsive Login and Signup Form </title>
-
-        <!-- CSS -->
-        <link rel="stylesheet" href="build/css/login.css">
-                
-        <!-- Boxicons CSS -->
-        <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
-                        
-    </head>
-    <body>
-        <section class="container forms">
-            <div class="form login">
-                <div class="form-content">
-                    <header>Login</header>
-                    <form action="#">
-                        <div class="field input-field">
-                            <input type="email" placeholder="Email" class="input">
-                        </div>
-
-                        <div class="field input-field">
-                            <input type="password" placeholder="Password" class="password">
-                            <i class='bx bx-hide eye-icon'></i>
-                        </div>
-
-                        <div class="form-link">
-                            <a href="#" class="forgot-pass">Forgot password?</a>
-                        </div>
-
-                        <div class="field button-field">
-                            <button>Login</button>
-                        </div>
-                    </form>
-
-                    <div class="form-link">
-                        <span>Don't have an account? <a href="#" class="link signup-link">Signup</a></span>
-                    </div>
-                </div>
-
-                <div class="line"></div>
-
-                <div class="media-options">
-                    <a href="#" class="field facebook">
-                        <i class='bx bxl-facebook facebook-icon'></i>
-                        <span>Login with Facebook</span>
-                    </a>
-                </div>
-
-                <div class="media-options">
-                    <a href="#" class="field google">
-                        <img src="#" alt="" class="google-img">
-                        <span>Login with Google</span>
-                    </a>
-                </div>
-
-            </div>
-
-            <!-- Signup Form -->
-
-            <div class="form signup">
-                <div class="form-content">
-                    <header>Signup</header>
-                    <form action="#">
-                        <div class="field input-field">
-                            <input type="email" placeholder="Email" class="input">
-                        </div>
-
-                        <div class="field input-field">
-                            <input type="password" placeholder="Create password" class="password">
-                        </div>
-
-                        <div class="field input-field">
-                            <input type="password" placeholder="Confirm password" class="password">
-                            <i class='bx bx-hide eye-icon'></i>
-                        </div>
-
-                        <div class="field button-field">
-                            <button>Signup</button>
-                        </div>
-                    </form>
-
-                    <div class="form-link">
-                        <span>Already have an account? <a href="#" class="link login-link">Login</a></span>
-                    </div>
-                </div>
-
-                <div class="line"></div>
-
-                <div class="media-options">
-                    <a href="#" class="field facebook">
-                        <i class='bx bxl-facebook facebook-icon'></i>
-                        <span>Login with Facebook</span>
-                    </a>
-                </div>
-
-                <div class="media-options">
-                    <a href="#" class="field google">
-                        <img src="#" alt="" class="google-img">
-                        <span>Login with Google</span>
-                    </a>
-                </div>
-
-            </div>
-        </section>
-
-        <!-- JavaScript -->
-        <script src="jquery/jquery.js"></script>
-    </body>
-</html>
-
 <?php
+    session_start();    
+    if (isset($_SESSION['usuario']))
+    {
+        header("location: bienvenida.php");
+    }
+    
 
-// Conectar a la base de datos (reemplaza los valores con los tuyos)
-$servername = "localhost";
-$username = "root";
-$dbname = "tuhogar";
-
-$conn = new mysqli($servername, $username, $dbname );
-
-// Verificar la conexión
-if ($conn->connect_error) {
-    die("Error de conexión a la base de datos: " . $conn->connect_error);
-}
-
-// Verificar reCAPTCHA
-$recaptcha_secret = "TU_CLAVE_SECRETA_RECAPTCHA";
-$recaptcha_response = $_POST['g-recaptcha-response'];
-
-$recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
-$recaptcha_data = [
-    'secret' => $recaptcha_secret,
-    'response' => $recaptcha_response,
-];
-
-$options = [
-    'http' => [
-        'header' => "Content-type: application/x-www-form-urlencoded\r\n",
-        'method' => 'POST',
-        'content' => http_build_query($recaptcha_data),
-    ],
-];
-
-$context = stream_context_create($options);
-$recaptcha_result = file_get_contents($recaptcha_url, false, $context);
-$recaptcha_data = json_decode($recaptcha_result);
-
-if (!$recaptcha_data->success) {
-    die("reCAPTCHA no válido. Vuelve e inténtalo de nuevo.");
-}
-
-// Procesar el formulario de inicio de sesión
-$username = $_POST['username'];
-$password = $_POST['password'];
-
-$sql = "SELECT * FROM usuarios WHERE username = '$username' AND password = '$password'";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    echo "Inicio de sesión exitoso para $username";
-} else {
-    echo "Credenciales incorrectas";
-}
-
-$conn->close();
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro Tu Hogar 360°</title>
+    
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+
+
+    <link rel="stylesheet" href="assets/css/estilos.css">
+</head>
+<body>
+
+        <main>
+
+            <div class="contenedor__todo">
+                <div class="caja__trasera">
+                    <div class="caja__trasera-login">
+                        <h3>¿Ya tienes una cuenta?</h3>
+                        <p>Inicia sesión para entrar en la página</p>
+                        <button id="btn__iniciar-sesion">Iniciar Sesión</button>
+                    </div>
+                    <div class="caja__trasera-register">
+                        <h3>¿Aún no tienes una cuenta?</h3>
+                        <p>Regístrate para que puedas iniciar sesión</p>
+                        <button id="btn__registrarse">Regístrarse</button>
+                    </div>
+                </div>
+
+                <!--Formulario de Login y registro-->
+                <div class="contenedor__login-register">
+                    <!--Login-->
+                    <form action="" class="formulario__login">
+                        <h2>Iniciar Sesión</h2>
+                        <input type="text" placeholder="Correo Electronico">
+                        <input type="password" placeholder="Contraseña">
+                        <button>Entrar</button>
+                    </form>
+
+                    <!--Register-->
+                    <form action="php/registro_usuario_be.php" method="POST" class="formulario__register">
+                        <h2>Regístrarse</h2>
+                        <input type="text" placeholder="Nombre completo" name="nombre_completo">
+                        <input type="text" placeholder="Correo Electronico" name="correo">
+                        <input type="text" placeholder="Usuario" name="usuario">
+                        <input type="password" placeholder="Contraseña" name="contrasena">
+                        <button>Regístrarse</button>
+                    </form>
+                </div>
+            </div>
+
+        </main>
+
+        <script src="assets/js/script.js"></script>
+</body>
+</html>
---
+++ php/conexion_be.php
@@ -0,0 +1,13 @@
+<?php 
+
+
+$conexion = mysqli_connect("localhost", "root", "", "login_hogar360");
+/*
+if($conexion){
+    echo 'conectado exitosamente a la bd';
+}else{
+    'no se ha podido conectar a la bd';
+}
+ */
+
+?>
