e855c35f3aea92d7af4061df746c1d1bcde833c1
Addivasqz <111457255+Addivasqz@users.noreply.github.com>
2024-03-21 12:52:42 -0600

    Create banner.php

---
+++ banner.php
@@ -0,0 +1,4 @@
+<div class="banner">
+    <img src="file:///C:/Users/irvin/Downloads/Presentaci%C3%B3n%20Inmobiliaria%20Casas%20Elegante%20Verde.png" alt="Banner Image">
+    <button><a href="enlace-del-botón.php">Ver más</a></button>
+</div>
